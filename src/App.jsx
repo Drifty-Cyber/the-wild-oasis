3f031063f723285a417833d5a9b511eba456d995
Drifty-Cyber <raphaelfadimu@gmail.com>
2024-02-24 15:09:39 +0100

    fix: fix flex issue

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,8 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
-      staleTime: 60 * 1000,
+      // staleTime: 60 * 1000,
+      staleTime: 0,
     },
   },
 });
@@ -45,7 +46,5 @@ function App() {
     </QueryClientProvider>
   );
 }
-
-// REMEMBER Download React Query DevTools
 
 export default App;
