585059c7024b25e51a86d34a9ba8e7c9ea64b7b1
Drifty-Cyber <raphaelfadimu@gmail.com>
2024-02-28 13:10:01 +0100

    chore: add edit cabin functionality

--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -11,19 +11,32 @@ export async function getCabins() {
   return data;
 }
 
-export async function createCabin(newCabin) {
+export async function createEditCabin(newCabin, id) {
+  const hasImagePath = newCabin.image?.startsWith?.(supabaseUrl);
+
   const imageName = `${Math.random()}-${newCabin.image.name}`.replaceAll(
     "/",
     ""
   );
 
-  const imagePath = `${supabaseUrl}/storage/v1/object/public/cabin-images/${imageName}`;
+  const imagePath = hasImagePath
+    ? newCabin.image
+    : `${supabaseUrl}/storage/v1/object/public/cabin-images/${imageName}`;
 
-  // Create Cabin
-  const { data, error } = await supabase
-    .from("cabins")
-    .insert([{ ...newCabin, image: imagePath }])
-    .select();
+  //1. Create/Edit Cabin
+  let query = supabase.from("cabins");
+
+  //A) Create
+  if (!id) query = query.insert([{ ...newCabin, image: imagePath }]);
+
+  //B) Update
+  if (id)
+    query = query
+      .update({ ...newCabin, image: imagePath })
+      .eq("id", id)
+      .select();
+
+  const { data, error } = await query.select().single();
 
   if (error) {
     console.error(error);
